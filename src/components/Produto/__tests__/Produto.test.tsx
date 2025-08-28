a90ca09601af2be1b4f997c347867da39e15a888
Danilo Jesús <danilojesus112001@gmail.com>
2025-08-27 23:46:52 -0300

    chore: add test for the Product component

---
+++ src/components/Produto/__tests__/Produto.test.tsx
@@ -0,0 +1,31 @@
+import { screen } from '@testing-library/react'
+import Produto from '..'
+import { renderizaComProvider } from '../../../utils/tests'
+
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+
+const jogo = {
+  id: 2,
+  categoria: 'RPG',
+  imagem: '',
+  plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+  preco: 199.9,
+  precoAntigo: 299.9,
+  titulo: 'Hogwarts Legacy'
+}
+
+describe('Testes para o componente produto', () => {
+  test('Deve renderizar corretamente', () => {
+    renderizaComProvider(<Produto game={jogo} />)
+    expect(screen.getByText('Hogwarts Legacy')).toBeInTheDocument()
+  })
+
+  test('Deve adicionar um item ao carrinho', async () => {
+    const { store } = renderizaComProvider(<Produto game={jogo} />)
+    const botao = screen.getByTestId('btn-adicionar-produto')
+    await userEvent.click(botao)
+
+    expect(store.getState().carrinho.itens).toHaveLength(1)
+  })
+})
