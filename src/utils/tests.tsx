4ffebb6d63b521b13fbbe5c4f3a4d4e79db0abdb
Danilo Jesús <danilojesus112001@gmail.com>
2025-08-27 22:57:02 -0300

    chore: tests with react and redux

--- src/utils/tests.tsx
+++ src/utils/tests.tsx
@@ -1,15 +1,33 @@
 import { PreloadedState } from '@reduxjs/toolkit'
-import { RenderOptions } from '@testing-library/react'
+import { RenderOptions, render } from '@testing-library/react'
 
-type ExtendedRenderOptions = Omit<RenderOptions, 'queries'>{
-  preloadedState?: PreloadedState
+import { PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+import { AppStore, RootState, configuraStore } from '../store'
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>
+  store?: AppStore
 }
 
 export function renderizaComProvider(
   elemento: React.ReactElement,
   {
     preloadedState = {},
-    store,
+    store = configuraStore(preloadedState),
     ...opcpesAdicionais
   }: ExtendedRenderOptions = {}
-) {}
+) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  function Encapsulador({ children }: PropsWithChildren<{}>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>
+  }
+
+  return {
+    store,
+    ...render(elemento, {
+      wrapper: Encapsulador,
+      ...opcpesAdicionais
+    })
+  }
+}
